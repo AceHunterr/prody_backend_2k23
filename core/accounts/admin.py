291fada1473f41837a55e3ab27977bb134ee0fc3
AceHunterr <aggarwalmehul26@gmail.com>
2023-09-14 22:19:43 +0530

    Added registered events per user in admin panel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_id',
+                    'get_registered_events', 'get_team_in_event')
+
+    def get_registered_events(self, obj):
+        return ", ".join([event.name for event in obj.events_registered.all()])
+
+    def get_team_in_event(self, obj):
+        return ", ".join([team.name for team in obj.teams.all()])
+
+    get_registered_events.short_description = 'Registered Events'
+    get_team_in_event.short_description = 'Team in Event'
+
+
+# Register the CustomUser model with the CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
