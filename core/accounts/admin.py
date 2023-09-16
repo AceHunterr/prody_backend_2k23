6949c4aa605f5f24e6d16f3973525e100f732d2d
AceHunterr <aggarwalmehul26@gmail.com>
2023-09-16 23:46:06 +0530

    User-event relationship from admin panel finally working lmao

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -13,5 +13,27 @@ class CustomUserAdmin(ImportExportModelAdmin):
 
     get_registered_events.short_description = 'Registered Events'
 
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+
+        # Get the updated set of registered events after saving
+        updated_registered_events = set(form.instance.registered_events.all())
+
+        # Find events that were removed from registration
+        removed_events = set(
+            form.initial['registered_events']) - updated_registered_events
+
+        # Find events that were added to registration
+        added_events = updated_registered_events - \
+            set(form.initial['registered_events'])
+
+        # Remove user from removed events' registered_users
+        for event in removed_events:
+            event.registered_users.remove(form.instance)
+
+        # Add user to added events' registered_users
+        for event in added_events:
+            event.registered_users.add(form.instance)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
