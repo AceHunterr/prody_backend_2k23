3b149ec944bf1c645e8c8bd8ce581d311c83d5e8
AceHunterr <aggarwalmehul26@gmail.com>
2023-09-16 23:56:30 +0530

    Vice versa bhi ho gaya

--- core/events/admin.py
+++ core/events/admin.py
@@ -11,6 +11,28 @@ class EventAdmin(ImportExportModelAdmin):
 
     get_registered_users.short_description = 'Registered Users'
 
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+
+        # Get the updated set of registered users after saving
+        updated_registered_users = set(form.instance.registered_users.all())
+
+        # Find users that were removed from registration
+        removed_users = set(
+            form.initial['registered_users']) - updated_registered_users
+
+        # Find users that were added to registration
+        added_users = updated_registered_users - \
+            set(form.initial['registered_users'])
+
+        # Remove event from removed users' registered_events
+        for user in removed_users:
+            user.registered_events.remove(form.instance)
+
+        # Add event to added users' registered_events
+        for user in added_users:
+            user.registered_events.add(form.instance)
+
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Team)
